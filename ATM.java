ce765e1d90570547e3c923840752a60263c97202
Allie <allie.hunnius@gmail.com>
2023-08-31 10:24:34 -0700

    openAccount

---
+++ ATM.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+public class ATM {
+
+    private HashMap <String,  Account> hm;
+
+    public ATM ()
+    {
+        
+        hm = new HashMap <String, Account> ();
+
+    }
+
+
+
+    public void createAccount (String email, double amount)
+    {
+        if (!hm.containsKey(email))
+        {
+            hm.put (email, new Account(amount));
+        }
+
+
+    }
+}
