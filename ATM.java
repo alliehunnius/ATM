c05eddd3e51e7418768e7a49ddbbfce2aecb9c3f
Allie <allie.hunnius@gmail.com>
2023-08-31 10:32:44 -0700

    checkBalance

--- ATM.java
+++ ATM.java
@@ -13,16 +13,30 @@ public class ATM {
 
 
 
-    public void createAccount (String email, double amount)
+    public void createAccount (String email, double amount) throws Exception
     {
         if (!hm.containsKey(email))
         {
             hm.put (email, new Account(amount));
         }
-        else{
+        else
+        {
             throw new Exception ("This user already has an account.");
         }
-
-
     }
+
+    public double checkBalance (String email) throws Exception
+    {
+        if (hm.containsKey (email))
+        {
+            return hm.get (email).getBalance();
+        }
+        else
+        {
+            throw new Exception ("No account can be found under this email.");
+        }
+        
+    }
+    
+}
 
